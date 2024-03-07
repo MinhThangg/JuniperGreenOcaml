fc513f2d1879ac332b3eee985f66fd7894008039
MinhThangg <43386211+MinhThangg@users.noreply.github.com>
2024-03-07 12:24:33 +0100

    Update untitled (5).ml

--- untitled (5).ml
+++ untitled (5).ml
@@ -2,24 +2,18 @@
 type cell_state = Unchecked | Checked;;
 type cell = Cell of int * cell_state;;
 type player = Player of int;;
+exception CoupImpossible;;
 
-let ligne = 10;;
-let colonne = 10;;
-let size = ligne*colonne;;
-
-let nb_of_char n = String.length (string_of_int n);;
 let char_per_cell = nb_of_char size;;
 
 let creer_plateau ligne colonne = 
   Array.init (ligne*colonne) (fun i -> Cell(i+1, Unchecked));;
 
-
-
 let affiche_plateau plt = 
   for i=0 to ligne-1 do
     print_string "|";
     for j=0 to colonne-1 do
-      plt.(3)<-Cell(5, Checked);
+      
       
       (match (plt.(j+i*colonne)) with 
         | Cell(idx, Unchecked) -> (
@@ -37,16 +31,33 @@ let affiche_plateau plt =
     print_endline ""
   done;;
 
+let mult_or_divi a b =
+  if b <> 0 && a <> 0 then
+    a mod b = 0 || b mod a = 0;
+  else
+    false;
+
+let ligne = 5;;
+let colonne = 5;;
+let size = ligne*colonne;;
+
 let plateau = creer_plateau ligne colonne
 let last = ref 0;;
+let nb_of_char n = String.length (string_of_int n);;
+
+let player_list = [|Player(1), Player(2)|];;
+let last_player_idx = 0;;
 
 let jouer plt idx = 
-  plt.(3)<-Cell(5, Checked);
+  if !last = 0 || (mult_or_divi idx !last) then (
+    plt.(idx-1)<-Cell(idx+1, Checked);
+    last := idx;
+  else
+    print_string "Coup impossible";
+  
   (affiche_plateau plt);;
 
 jouer plateau 4;;
 
-
-affiche_plateau (creer_plateau ligne colonne);;
 
 
